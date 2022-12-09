68c34a0212be8eb078c9fff572c9cfa078b01aa3
prokden <prokden@gmail.com>
2022-12-09 23:45:38 +0300

    Answer field relised

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,9 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: %i[show destroy edit update]
+  before_action :set_question!, only: %i[show destroy edit update]
 
   def show
     @answer = @question.answers.build
+    @answers = @question.answers.order created_at: :desc
   end
   
   def destroy
@@ -47,7 +48,7 @@ class QuestionsController < ApplicationController
     params.require(:question).permit(:title, :body)
   end
 
-  def set_question
+  def set_question!
     @question = Question.find params[:id]
   end
 end

eddb8e29c7220683675dccde0b04fa7783dc4755
prokden <prokden@gmail.com>
2023-01-19 21:35:55 +0300

    Log out

--- Myapp/app/controllers/concerns/authentication.rb
+++ Myapp/app/controllers/concerns/authentication.rb
@@ -12,6 +12,29 @@ module Authentication
       current_user.present?
     end
 
+    def require_authentication
+      return if user_signed_in?
+
+      flash[:warning] = "You are not signed in!"
+      redirect_to root_path
+    end
+
+    def require_no_authentication
+      return if !user_signed_in?
+
+      flash[:warning] = "You are already signed in!"
+      redirect_to root_path
+    end
+      
+    
+    def sign_in(user)
+      session[:user_id] = user.id
+    end
+
+    def sign_out
+      session.delete :user_id
+    end
+
     helper_method :current_user, :user_signed_in?
     end
   end
--- Myapp/app/controllers/users_controller.rb
+++ Myapp/app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :require_no_authentication
+  before_action :require_authentication, only: :destroy
+
+  def edit
+  end
+
+  def update
+  end
+  
   def new
     @user = User.new
   end
@@ -6,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      session[:user_id] = @user.id
+      sign_in @user
       flash[:success] = "Welcome to the app, #{current_user.name_or_email}!"
       redirect_to root_path
     else

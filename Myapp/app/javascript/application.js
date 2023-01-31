f2a92408832697979f7092d756ee1e9eeb44aeb0
prokden <prokden@gmail.com>
2023-01-31 18:08:49 +0300

    Lesson13

--- Myapp/app/javascript/application.js
+++ Myapp/app/javascript/application.js
@@ -4,7 +4,9 @@ import Turbolinks from "turbolinks";
 
 import'@popperjs/core'
 import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/collapse'
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+import * as bootstrap from "bootstrap"

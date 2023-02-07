0e1e7faff2a9464153534a6a5985464a4f0dd195
prokden <prokden@gmail.com>
2023-02-07 17:51:23 +0300

    Lesson13

--- Myapp/config/application.rb
+++ Myapp/config/application.rb
@@ -26,7 +26,7 @@ module Myapp
     config.load_defaults 7.0
 
     config.i18n.available_locales = %i[en ru]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ru
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

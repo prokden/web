23ac3a4f67c42cdad730926029122577134e48ed
prokden <prokden@gmail.com>
2023-02-07 22:38:15 +0300

    Lesson14 gravatar

--- Myapp/app/models/user.rb
+++ Myapp/app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
 
   has_secure_password validations: false
 
+  has_many :questions, dependent: :destroy
+  has_many :answers, dependent: :destroy
+
   validate :password_presence
   validate :correct_old_password, on: :update, if: -> { password.present? }
   validates :password, confirmation: true, allow_blank: true,
--- Myapp/db/schema.rb
+++ Myapp/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_23_193347) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_190000) do
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.integer "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -24,6 +26,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_23_193347) do
     t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_23_193347) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
+  add_foreign_key "questions", "users"
 end
